bee1e19069e1ea12caf4e9e2dbc99af2892f1cb2
Astral Maria <rsteer67@gmail.com>
2018-02-03 16:03:21 +0000

    no message

--- Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void calculateScore(View view) {
+
+
         //There's no need to assign new Booleans to the states of the checkboxes;
         //We can use them as arguments for if statements, since their datatype defaults to bool
 
@@ -49,17 +53,67 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //Question 3's Logic
+        int question3 = 0;
+        CheckBox a3 = (CheckBox) findViewById(R.id.a3);
+        CheckBox b3 = (CheckBox) findViewById(R.id.b3);
+        CheckBox c3 = (CheckBox) findViewById(R.id.c3);
+
+        if (a3.isChecked() && !b3.isChecked() && !c3.isChecked()) {
+            question3 ++;
+        }
+        else {
+            if (question3 != 0) {
+                question3--;
+            }
+        }
+//Question 4's Logic
+        int question4 = 0;
+        CheckBox a4 = (CheckBox) findViewById(R.id.a4);
+        CheckBox b4 = (CheckBox) findViewById(R.id.b4);
+        CheckBox c4 = (CheckBox) findViewById(R.id.c4);
+
+        if (!a4.isChecked() && !b4.isChecked() && c4.isChecked()) {
+            question4 ++;
+        }
+        else {
+            if (question4 != 0) {
+                question4--;
+            }
+        }
         //To add more Checkboxes you can copy the above blocks of code and link new id's of new checkboxes
         //Then just sum them up in the score argument below
 
         TextView scoreOutput = findViewById(R.id.scoreTextView);
-        String messageScore = createScoreSummary(question1 + question2, scoreOutput);
+        String messageScore = createScoreSummary(question1 + question2 + question3 + question4, scoreOutput);
 
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioMr:
+                if (checked)
+
+                    break;
+            case R.id.radioMs:
+                if (checked)
+
+                    break;
+        }
+    }
     private String createScoreSummary (int Score, TextView txt) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
+
+
         String goodJob = getString(R.string.goodJob);
-        String scoreMessage = "\n" + getString(R.string.scoreMessage) + ": " + String.valueOf(Score);
+        String scoreMessage = "\n" + getString(R.string.scoreMessage) + ": " + String.valueOf(Score)
+                + " out of 4! " +" " + goodJob + " " + name + "!";
         txt.setText(scoreMessage);
         return getString(R.string.scoreMessage);
     }
