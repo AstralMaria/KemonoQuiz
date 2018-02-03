d254e4cb3512e8856fc0365d23117eb2464eff1f
Kari Barry <kevin.a.barry@stonybrook.edu>
2018-02-03 02:55:20 -0500

    I see the general idea you were going for, it's a good attempt at branching logic some of the neat tricks in modern programming language is that props often default to a certain datatype; the .isChecked() prop can be used as a boolean for if statements which streamlines the code significantly. Also using incriminators ++, decrements -- to tally score. I wasn't sure what I should do to add more checkboxes but it will be simple to copy the logic written here and duplicate them, assigning new ID's as you go.

--- Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,54 +16,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void calculateScore(View view) {
+        //There's no need to assign new Booleans to the states of the checkboxes;
+        //We can use them as arguments for if statements, since their datatype defaults to bool
 
-
+        //Question 1's Logic
+        int question1 = 0;
         CheckBox a1 = (CheckBox) findViewById(R.id.a1);
         CheckBox b1 = (CheckBox) findViewById(R.id.b1);
         CheckBox c1 = (CheckBox) findViewById(R.id.c1);
+
+        if (b1.isChecked() && !a1.isChecked() && !c1.isChecked()) {
+            question1 ++;
+        }
+        else {
+            if (question1 !=0){
+                question1 --;
+            }
+        }
+
+        //Question 2's Logic
+        int question2 = 0;
         CheckBox a2 = (CheckBox) findViewById(R.id.a2);
         CheckBox b2 = (CheckBox) findViewById(R.id.b2);
         CheckBox c2 = (CheckBox) findViewById(R.id.c2);
 
-        boolean a1BoolCheck = a1.isChecked();
-        boolean a2BoolCheck = b1.isChecked();
-        boolean a3BoolCheck = c1.isChecked();
-        boolean b1BoolCheck = a2.isChecked();
-        boolean b2BoolCheck = b2.isChecked();
-        boolean b3BoolCheck = c2.isChecked();
-        int score = calculateScore(a1BoolCheck, a2BoolCheck, a3BoolCheck, b1BoolCheck, b2BoolCheck, b3BoolCheck);
-        String messageScore = createScoreSummary(score, a1BoolCheck, a2BoolCheck, a3BoolCheck, b1BoolCheck, b2BoolCheck, b3BoolCheck);
-        displayMessage(messageScore);
+        if (a2.isChecked() && !b2.isChecked() && !c2.isChecked()) {
+            question2 ++;
+        }
+        else {
+            if (question2 != 0) {
+                question2--;
+            }
+        }
+
+        //To add more Checkboxes you can copy the above blocks of code and link new id's of new checkboxes
+        //Then just sum them up in the score argument below
+
+        TextView scoreOutput = findViewById(R.id.scoreTextView);
+        String messageScore = createScoreSummary(question1 + question2, scoreOutput);
+
     }
 
-    private String createScoreSummary (int Score, boolean a1BoolCheck, boolean a2BoolCheck, boolean a3BoolCheck,
-                                       boolean b1BoolCheck, boolean b2BoolCheck, boolean b3BoolCheck) {
+    private String createScoreSummary (int Score, TextView txt) {
         String goodJob = getString(R.string.goodJob);
-        String scoreMessage = "\n" + getString(R.string.scoreMessage) + ": " + Score;
+        String scoreMessage = "\n" + getString(R.string.scoreMessage) + ": " + String.valueOf(Score);
+        txt.setText(scoreMessage);
         return getString(R.string.scoreMessage);
     }
-    private int calculateScore ( boolean a1Bool, boolean a2Bool, boolean a3Bool,
-                                 boolean b1Bool, boolean b2Bool, boolean b3Bool) {
-        int baseScore = 0;
 
-        if (!a1Bool && a2Bool && !a3Bool) {
-            baseScore = baseScore + 2;
-        }
-
-        else if (b1Bool && !b2Bool && !b3Bool ) {
-            baseScore = baseScore +2;
-        }
-
-        else {
-            baseScore = baseScore;
-        }
-        baseScore=baseScore;
-        return baseScore;
-    }
-
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = findViewById(R.id.scoreTextView);
-        orderSummaryTextView.setText(message);
-    }
 }
